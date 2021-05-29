b0be4208c3574d0f8aad2c3d976ad2b3d659cfbb
Aber <me@abersheeran.com>
2021-05-29 23:23:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ fi
 sed -i "s/uuid/${uuid}/" /src/v2ray.json
 
 echo "安卓客户端 v2rayNG 配置链接："
-echo "vmess://\b"
+echo -e "vmess://\c"
 echo "{""add":"${domain}","aid":"64","host":"","id":"${uuid}","net":"ws","path":"${path}","port":"443","ps":"${domain}","tls":"tls","type":"none","v":"2""}" | base64
 
 /usr/bin/v2ray -config /src/v2ray.json
