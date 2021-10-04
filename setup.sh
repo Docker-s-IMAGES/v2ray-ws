47659c4dcb5b06c11a88f3dc8ba2f39e2e5efd84
Aber <me@abersheeran.com>
2021-10-04 23:43:14 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,8 +16,16 @@ fi
 
 sed -i -e "s@UUID@${uuid}@" -e "s@PATH@${path}@" /src/v2ray.json
 
+echo "域名："$domain
+echo "UUID："$uuid
+echo "路径："$path
+echo ""
 echo "安卓客户端 v2rayNG 配置链接："
 echo -e "vmess://\c"
 echo '{"add":"DOMAIN","aid":"64","host":"","id":"UUID","net":"ws","path":"PATH","port":"443","ps":"DOMAIN","sni":"","tls":"tls","type":"none","v":"2"}' | sed -e "s@DOMAIN@${domain}@" -e "s@DOMAIN@${domain}@" -e "s@PATH@${path}@" -e "s@UUID@${uuid}@" | base64
+echo "电脑客户端 v2rayN 配置链接："
+echo -e "vmess://\c"
+echo '{"v": "2", "ps": "", "add": "DOMAIN", "port": "443", "id": "UUID", "aid": "64", "scy": "none", "net": "ws", "type": "none", "host": "", "path": "PATH", "tls": "tls", "sni": "DOMAIN"}' | sed -e "s@DOMAIN@${domain}@" -e "s@DOMAIN@${domain}@" -e "s@PATH@${path}@" -e "s@UUID@${uuid}@" | base64
+
 
 /usr/bin/v2ray -config /src/v2ray.json
